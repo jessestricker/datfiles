4f7841d5b26c4247d0788b77092b48df7aaed867
Jesse Stricker <git@jessestricker.de>
2026-01-19 20:08:42 +0100

    feat: scrape from redump

--- _scraper/main.py
+++ _scraper/main.py
@@ -1,9 +1,26 @@
+import argparse
 import logging
+import shutil
 import sys
+from pathlib import Path
+
+import redump
 
 
 def main() -> None:
     logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--clean", action="store_true")
+    parser.add_argument("output_dir")
+    args = parser.parse_args()
+
+    output_dir = Path(args.output_dir)
+
+    redump_dir = output_dir / "redump"
+    if args.clean and redump_dir.exists():
+        shutil.rmtree(redump_dir)
+    redump.scrape(redump_dir)
 
 
 if __name__ == "__main__":
---
+++ _scraper/redump.py
@@ -0,0 +1,160 @@
+import logging
+import shutil
+import tempfile
+import zipfile
+from dataclasses import dataclass
+from pathlib import Path
+from urllib.parse import urljoin
+
+import bs4
+import requests
+from pathvalidate import sanitize_filename
+
+_BASE_URL = "http://redump.org"
+
+_logger = logging.getLogger(__name__)
+_session = requests.Session()
+
+
+def scrape(output_dir: Path) -> None:
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    _logger.info("fetching systems...")
+    systems = _get_systems()
+
+    datfile_count = sum(system.datfile_url is not None for system in systems)
+    bios_datfile_count = sum(system.bios_datfile_url is not None for system in systems)
+    _logger.info(
+        "fetched %s systems with %s datfiles and %s bios datfiles",
+        len(systems),
+        datfile_count,
+        bios_datfile_count,
+    )
+
+    _logger.info("downloading datfiles...")
+    system_count = len(systems)
+
+    with tempfile.TemporaryDirectory() as td:
+        temp_dir = Path(td)
+
+        for index, system in enumerate(systems):
+            _logger.info(
+                f"(%{len(str(system_count))}s/%s) %s",  # noqa: G004
+                index + 1,
+                len(systems),
+                system.name,
+            )
+
+            if system.datfile_url is not None:
+                target_file = output_dir / sanitize_filename(f"{system.name}.xml")
+                _download_datfile(system.datfile_url, target_file, temp_dir)
+
+            if system.bios_datfile_url is not None:
+                target_file = output_dir / sanitize_filename(f"{system.name}_BIOS.dat")
+                _download_datfile(system.bios_datfile_url, target_file, temp_dir)
+
+    _logger.info("downloaded datfiles")
+
+
+@dataclass
+class _System:
+    name: str
+    datfile_url: str | None
+    bios_datfile_url: str | None
+
+
+def _get_systems() -> list[_System]:
+    systems: list[_System] = []
+
+    resp = _session.get(urljoin(_BASE_URL, "downloads"))
+    resp.raise_for_status()
+    html = resp.text
+    soup = bs4.BeautifulSoup(html, "lxml")
+
+    table_rows = soup.css.select("#main table tr")
+    header_tr, *body_trs = table_rows
+
+    name_index, datfile_index, bios_datfile_index = _find_column_indices(header_tr)
+
+    for tr in body_trs:
+        tds = tr.find_all("td")
+
+        name = tds[name_index].string
+        if name is None:
+            msg = "<td> for name is empty"
+            raise ValueError(msg)
+
+        datfile_url = _get_cell_link_url(tds[datfile_index])
+        bios_datfile_url = _get_cell_link_url(tds[bios_datfile_index])
+
+        system = _System(
+            name=name,
+            datfile_url=datfile_url,
+            bios_datfile_url=bios_datfile_url,
+        )
+        systems.append(system)
+
+    return systems
+
+
+def _find_column_indices(tr: bs4.Tag) -> tuple[int, int, int]:
+    systems_th = tr.find("th", text="Systems")
+    if systems_th is None:
+        msg = "<th>Systems</th> not found"
+        raise ValueError(msg)
+
+    datfiles_th = tr.find("th", text="Datfiles")
+    if datfiles_th is None:
+        msg = "<th>Datfiles</th> not found"
+        raise ValueError(msg)
+
+    bios_datfiles_th = tr.find("th", text="BIOS Datfiles")
+    if bios_datfiles_th is None:
+        msg = "<th>BIOS Datfiles</th> not found"
+        raise ValueError(msg)
+
+    return (
+        tr.index(systems_th),
+        tr.index(datfiles_th),
+        tr.index(bios_datfiles_th),
+    )
+
+
+def _get_cell_link_url(td: bs4.Tag) -> str | None:
+    link = td.find("a")
+    if link is None:
+        return None
+
+    href = link["href"]
+    if not isinstance(href, str):
+        return None
+    return href
+
+
+def _download_datfile(url_path: str, target_file: Path, temp_dir: Path) -> None:
+    # download to temp file
+    url = urljoin(_BASE_URL, url_path)
+    temp_file = temp_dir / sanitize_filename(url_path)
+    _logger.info("downloading %s -> %s ...", url, temp_file)
+
+    with temp_file.open(mode="wb") as fd, _session.get(url, stream=True) as resp:
+        resp.raise_for_status()
+        for chunk in resp.iter_content(chunk_size=512):
+            fd.write(chunk)
+
+    # extract/copy to target file
+    if zipfile.is_zipfile(temp_file):
+        _extract_zipped_file(temp_file, target_file)
+    else:
+        temp_file.copy(target_file)
+
+
+def _extract_zipped_file(path: Path, target_file: Path) -> None:
+    with zipfile.ZipFile(path) as zip_file:
+        entry_names = zip_file.namelist()
+        if len(entry_names) != 1:
+            msg = "zip file does not contain a single file"
+            raise ValueError(msg)
+
+        with zip_file.open(entry_names[0]) as src, target_file.open(mode="wb") as dst:
+            shutil.copyfileobj(src, dst)
